f97fd223fb81b3060ff950aba2932e8ed2a521c4
nermin0327 <nerminseyidehmedova512@gmail.com>
2022-11-03 15:38:05 +0400

    bitdi allaha sukr

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const input = document.querySelector(".input");
 const ulList = document.querySelector("#myUL");
 const imgtwo = document.querySelector(".imgtwo");
 const sortButton2 = document.querySelector('.img2')
+const sortButton = document.querySelector('.imgone')
 
 
 addTo.addEventListener("click",(e)=>{
@@ -19,11 +20,11 @@ ulList.addEventListener("click",(e)=>{
   }
 })
 
-const sortButton = document.querySelector('.img1')
+
 
 let liElements = document.getElementById('myUL')
 
-if(sortButton.addEventListener('click',()=>{
+sortButton?.addEventListener('click',()=>{
     sortButton2.style.display = 'block';
     sortButton.style.display = 'none';
     sortButton2.style.marginBottom = '20px';
@@ -39,96 +40,50 @@ if(sortButton.addEventListener('click',()=>{
 
         liElements.innerHTML += li
     })
-    ar.sort((c,t)=>{
-        if (c > t) {
-            return -1;
-            }
-            if (c < t) {
-              return 1;
-            }
-            // a bərabərdir b
-            return 0;
-    
-
-    })
- 
-
- }))
-      
-  
-if(sortButton2.addEventListener('click',()=>{
-    sortButton.style.display = 'none';
-    sortButton2.style.display = 'block'
-    // let liElements = document.getElementById('myUL')
-// sortButton2.addEventListener("click", () => {
-//     let ar = []
-//     const data = [...liElements?.children]
-//     data.forEach(item => {
-//         ar.push(item.innerHTML)
-//     })
-//     ar.sort();
-//     liElements.innerHTML = ""
-//     ar.forEach(item => {
-//         let li = `<li>${item}   </li>`
-
-//         liElements.innerHTML += li
-//     })
-//     ar.sort((c,t)=>{
-//         if (c < t) {
-//             return -1;
-//             }
-//             if (c > t) {
-//               return 1;
-//             }
-//             // a bərabərdir b
-//             return 0;
-    
-
-//     })
-
-})){
-
-}
-
-
-
- 
-
-
-
-
     // ar.sort((c,t)=>{
-    //     if (c < t) {
+    //     if (c > t) {
     //         return -1;
     //         }
-    //         if (c >t) {
+    //         if (c < t) {
     //           return 1;
     //         }
     //         // a bərabərdir b
     //         return 0;
-
-  
-    // })
     
 
-   
+    // })
+
+ 
+
+ })
+      
+  
+ sortButton2?.addEventListener('click',()=>{
+    sortButton.style.display = 'block';
+    sortButton2.style.display = 'none';
+    let ar = [];
+    const data = [...liElements.children];
+    data.forEach(item =>{
+      ar.push(item.innerHTML);
+    })
+    ar.sort();
+    ar.reverse()
+    liElements.innerHTML = '';
+    ar.forEach(item =>{
+      let li = `<li>${item}</li>`;
+      liElements.innerHTML += li;
+    })
+
+   console.log("wefwef")
+
+  sortButton.style.display = 'block';
+    sortButton2.style.display = 'none';
 
 
 
-// let arr = ["z","b","c"]
 
-// arr.sort((c,t)=>{
-//     if (c > t) {
-//         return -1;
-//         }
-//         if (c < t) {
-//           return 1;
-//         }
-//         // a bərabərdir b
-//         return 0;
-// })
 
-// console.log(arr)
+})
 
 
 
